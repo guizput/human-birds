cddd88d16a64ba9966de254e88912f68bb93fefe
Guillaume Duran <guillaumeduran2@gmail.com>
2025-01-29 15:24:51 +0100

    Vaidation ID Youtube

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,5 +1,7 @@
 import TextInput from "../ui/TextInput";
 import ToggleElement from "../ui/ToggleElement";
+import useYouTubeIdValidator from "../hooks/useYouTubeIdValidator";
+import { useEffect, useState } from "react";
 
 export function Video({ data }) {
   return (
@@ -17,18 +19,37 @@ export function Video({ data }) {
 }
 
 export function EditVideo({ content, setContent }) {
+  const { isValid, validateYouTubeId } = useYouTubeIdValidator();
+  const [id, setId] = useState(content.video.youtubeID);
+
+  useEffect(() => {
+    if (isValid) {
+      setContent({
+        ...content,
+        video: { ...content.video, youtubeID: id },
+      });
+    }
+  }, [isValid]);
+
+  const handleChange = (e) => {
+    validateYouTubeId(e.target.value);
+    setId(e.target.value);
+  };
+
   return (
     <ToggleElement title="Video">
       <TextInput
         title="Youtube ID"
-        value={content.video.youtubeID}
-        onChange={(e) =>
-          setContent({
-            ...content,
-            video: { ...content.video, youtubeID: e.target.value },
-          })
-        }
+        value={id}
+        onChange={(e) => handleChange(e)}
       />
+      {isValid !== null && (
+        <p
+          className={`mt-2 font-medium ${isValid ? "text-green-600" : "text-red-600"}`}
+        >
+          {isValid ? "✅ ID valide !" : "❌ ID invalide !"}
+        </p>
+      )}
     </ToggleElement>
   );
 }
